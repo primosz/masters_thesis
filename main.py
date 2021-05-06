acef89321642f9a20e1dc900500439810d135257
Primosz <valadin96@gmail.com>
2021-05-06 20:05:43 +0200

    Started working on mapping string rules to library objects

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from doggos.utils.membership_functions.membership_functions import generate_equa
 
 sys.path.append((path.abspath('../biblioteka/DoggOSFuzzy/doggos')))
 
+#define fuzzy sets and save into dict
 gausses = generate_equal_gausses(3, 0, 1)
 small = Type1FuzzySet(gausses[0])
 medium = Type1FuzzySet(gausses[1])
@@ -19,23 +20,32 @@ large = Type1FuzzySet(gausses[2])
 
 fuzzy_sets = {'small': small, 'medium': medium, 'large': large}
 
+#read dataset and normalize it
 df = pd.read_csv('Data Banknote Authentication.csv', sep=';')
 df_ar = df.values
 min_max_scaler = MinMaxScaler()
 df_scaled = min_max_scaler.fit_transform(df_ar)
 df = pd.DataFrame(df_scaled, columns=df.columns)
 
+#fuzzify dataset
 gen = FuzzyDecisionTableGenerator(fuzzy_sets, df)
 fuzzified_dataset = gen.fuzzify()
 print(fuzzified_dataset)
+
+#remove inconsistencies
 inc_rem = InconsistenciesRemover(fuzzified_dataset, list(fuzzified_dataset.columns)[:-1])
 decision_table, changed_decisions = inc_rem.inconsistenciesRemoving()
 print(decision_table)
 print(changed_decisions)
-reductor = Reductor(decision_table, True)
 
+#create reduct
+reductor = Reductor(decision_table, True)
 decision_table_with_reduct, features_number_after_reduct = reductor.worker(decision_table)
 print(decision_table_with_reduct)
+
+#induce rules
 rb = RuleBuilder(decision_table_with_reduct)
 rules = rb.induce_rules()
 print(rules)
+
+#map string rules to fuzzy rules with type-2 fuzzy sets
